a0c27331ff64417ec8d74ae84d01a33e88b3cd93
Stefani Signore <stefanisignore@gmail.com>
2023-07-25 17:30:16 -0300

    Primeira versao

---
+++ server/src/server.ts
@@ -0,0 +1,52 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+
+import { poolRoutes } from './routes/pool'
+import { userRoutes } from './routes/user'
+import { guessRoutes } from './routes/guess'
+import { gameRoutes } from './routes/game'
+import { authRoutes } from './routes/auth'
+
+
+async function bootstrap() {
+    const fastify = Fastify({
+        logger: true,
+    })
+
+    await fastify.register(cors, {
+        origin: true,
+    })
+
+    // fastify.get('/pools/count', async () => {
+    //     const pools = await prisma.pool.findMany({
+    //         where:{
+    //             code:{
+    //                 startsWith: 'S'
+    //             }
+    //         }
+    //     })
+    //     return { pools }
+    // })
+
+    // fastify.get('/pools/count', () => {
+    //     return { count: 123 }
+    // })
+
+    //Em produção isso precisa ser uma variável ambiente
+    await fastify.register(jwt, {
+        secret: 'nlwcopa',
+    })
+
+    await fastify.register(poolRoutes)
+    await fastify.register(userRoutes)
+    await fastify.register(guessRoutes)
+    await fastify.register(gameRoutes)
+    await fastify.register(authRoutes)
+
+
+
+    await fastify.listen({ port : 3333, /*host: '0.0.0.0'*/ })
+}
+
+bootstrap()
